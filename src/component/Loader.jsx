a50c9c971a34d70cefbfabedee9b506e84f1d49a
Clement Enjolras <git@clementenjolras.fr>
2024-04-24 21:16:53 +0200

    three fiber setup

---
+++ src/component/Loader.jsx
@@ -0,0 +1,10 @@
+import { Html } from "@react-three/drei";
+export default function Loader() {
+  return (
+    <Html>
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    </Html>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Loader from "../component/Loader";
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <section className="w-full h-screen relative">
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+        </Suspense>
+      </Canvas>
+    </section>
   );
 }
