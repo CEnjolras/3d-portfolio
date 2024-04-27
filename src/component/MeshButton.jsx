cf0d392a33123d39c209f7246d91565d10078de2
Clement Enjolras <git@clementenjolras.fr>
2024-04-27 23:26:12 +0200

    try

--- src/component/MeshButton.jsx
+++ src/component/MeshButton.jsx
@@ -8,7 +8,7 @@ export default function MeshButton({ children, to, ...props }) {
     <Html distanceFactor={10} {...props} >
         <div 
           onClick={() => { navigate(to); }}
-          className="z-100 absolute hover:cursor-pointer h-4 w-4 lg:h-3 lg:w-3 pointer-events-auto rounded-full bg-black bg-opacity-70 shadow-lg ring-2 ring-white ring-opacity-50" 
+          className="z-100 absolute hover:cursor-pointer h-4 w-4 lg:h-3 lg:w-3 pointer-events-auto rounded-full bg-black bg-opacity-80 shadow-lg ring-2 ring-white ring-opacity-70" 
         />
     </Html>
 
