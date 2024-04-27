1380246a708f06cc79b46ae6885147ada2f89e72
Clement Enjolras <git@clementenjolras.fr>
2024-04-27 22:57:07 +0200

    avatar

--- src/component/Message.jsx
+++ src/component/Message.jsx
@@ -1,12 +1,14 @@
+import avatar from '../assets/man.webp'
+
 export default function Message({children, className}) {
   return (
     <div className={`absolute w-[500px] max-w-[85%] bottom-32 left-0 right-0 m-auto z-20 ${className} text-white pointer-events-auto rounded-lg bg-black bg-opacity-70 shadow-lg ring-1 ring-black ring-opacity-5`}>
       <div className="p-4">
         <div className="flex items-start">
-          <div className="shrink-0 pt-0.5 w-11"> 
+          <div className="shrink-0 pt-0.5 w-12"> 
             <img
-              className="h-10 w-10 rounded-full"
-              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
+              className="h-11 w-11 rounded-full"
+              src={avatar}
               alt=""
             />
           </div>
